783c6b7771e1a4053fa2efadd386b9126d72c944
sejun1217 <sejun1217@gmail.com>
2023-09-19 14:16:54 +0000

    rfc-glue-read-table-parallel.py 3rd 230919 by sejun

--- rfc-glue-read-table-parallel.py
+++ rfc-glue-read-table-parallel.py
@@ -11,6 +11,7 @@ import pandas as pd
 import io
 import pyarrow as pa
 import pyarrow.parquet as pq
+import uuid
 
 # Input Your Environment
 ASHOST='' #modify
@@ -63,20 +64,25 @@ def _result_to_output(result):
     print("Result Count: "+str(len(result[out_table])))
     global Result_Rowcount; Result_Rowcount = len(result[out_table])
     
-    # get filed
-    data_field = []
+    # Make field Name and Type.
+    field_name = []
+    field_type = []
+    field_data = {}
     for line in result["FIELDS"]:
-        data_field.append(line["FIELDNAME"])
-    print("Field Count: "+str(len(data_field)))
-  
+        field_name.append(line["FIELDNAME"])
+        field_type.append(line["TYPE"])
+    print("Field Count: "+str(len(field_name)))
+    for i in range(len(field_name)):
+        field_data[str(field_name[i])] = field_type[i]
+       
     # body
     data = []
     data_err = []
     data_count = 0
     err_count = 0
     for line in result[out_table]:
-        raw_data = line["WA"].strip().split(Table_Delimiter)
-        if len(raw_data) == len(data_field):
+        raw_data = line["WA"].strip().split(Table_Delimeter)
+        if len(raw_data) == len(field_name):
             data_count += 1
             data.append(raw_data)
         else:
@@ -86,13 +92,24 @@ def _result_to_output(result):
     print("Data Count: "+str(data_count))             
     print("Error Count: "+str(err_count))
     print("Total Count: "+str(data_count+err_count))
-    pd_result = pd.DataFrame(data,columns=data_field)
+    pd_result = pd.DataFrame(data,columns=field_name)
 
+    #strip string data
+    pd_result = pd_result.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+    
+    #convert data type
+    for fieldname in field_name:
+        if(field_data[fieldname] == 'N'):
+          pd_result[fieldname] = pd_result[fieldname].astype('int',errors='ignore')
+        elif(field_data[fieldname] == 'F'):
+          pd_result[fieldname] = pd_result[fieldname].astype('float',errors='ignore')
+
+    
     # pandas to json or parquet
     if len(data_err) == 0:
         if file_format == 'json':
             out_buffer = io.StringIO()
-            pd_result.to_json(out_buffer,orient = 'records')
+            pd_result.to_json(out_buffer,orient = 'table')
         elif file_format == 'parquet':
             pd_table = pa.Table.from_pandas(pd_result)
             out_buffer = io.BytesIO()
@@ -126,9 +143,9 @@ def _call_rfc_function():
     print("----Begin of RFC---")
     x = 0
     global Result_Rowcount;Result_Rowcount = Table_Rowcount
-    while (x < maxEntities and Result_Rowcount == Table_Rowcount ):
-        result = conn.call(RFC_Function, QUERY_TABLE = READ_Table, DELIMITER = Table_Delimiter, ROWSKIPS=x, ROWCOUNT=Table_Rowcount)
-        global dataS3file; dataS3file = READ_Table+str(x)+"."+file_format
+    while ( x < maxEntities and Result_Rowcount == Table_Rowcount ):
+        result = conn.call(RFC_Function, QUERY_TABLE = READ_Table, DELIMITER = Table_Delimeter, ROWSKIPS=x, ROWCOUNT=Table_Rowcount)
+        global dataS3file; dataS3file = READ_Table+"_"+str(x)+"."+file_format
         _result_to_output(result)
         # print(Result_Rowcount)
         x = x + Table_Rowcount
